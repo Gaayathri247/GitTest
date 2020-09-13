ef8d512b523791e7d5a26e4b382efb469eca9d79
Gaayathri247 <gaayathrih247@gmail.com>
2020-09-13 12:02:16 +0530

    Jenkin setup

--- src/test/java/sampleTest/SampleTest.java
+++ src/test/java/sampleTest/SampleTest.java
@@ -1,23 +1,18 @@
-package gitTest;
+package sampleTest;
 
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
-import javax.imageio.ImageIO;
-
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.OutputType;
-import org.openqa.selenium.Point;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-public class GitTest {
+public class SampleTest {
 
 	@Test
 	public void loginTest() throws IOException
@@ -27,23 +22,21 @@ public class GitTest {
 			WebDriver wd = new ChromeDriver();
 			wd.manage().window().maximize();
 			
-			wd.get("https://www.google.com");
+			wd.get("https://www.bing.com");
 			WebElement input = wd.findElement(By.name("q"));
 			input.sendKeys("Chennai");
 			input.submit();
+			System.out.println(wd.getTitle());
 			
 		
 			
 			// Takes screenshot of the full page
 			
 			File src = ((TakesScreenshot)wd).getScreenshotAs(OutputType.FILE);
-			FileUtils.copyFile(src, new File("C:\\Users\\gaayah\\Desktop\\SeleniumWorkspace\\GitTest\\Screenshot\\abc.png"));
-	
-			wd.quit();
-			int i = 1/0;
+			FileUtils.copyFile(src, new File("C:\\Users\\gaayah\\Desktop\\SeleniumWorkspace\\ztryHere\\Black\\abc.png"));
+	wd.quit();
+	int i = 1/0;
 	}
 
 
-	}
-
-
+}
